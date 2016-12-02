5e81e4e63882af2a9f064df2d62624ac05cd9b56
khaled hesham <khaledhesham2017@hotmail.com>
2016-12-02 22:15:46 +0200

    post save and pick

---
+++ src/SocialAppGeneral/Like.java
@@ -0,0 +1,17 @@
+package SocialAppGeneral;
+
+/**
+ * Created by khaled hesham on 12/2/2016.
+ */
+public class Like extends PostAtachmment {
+    private int like;
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {//put 1 if put like put -1 if put dislike
+        this.like = like;
+    }
+
+}
