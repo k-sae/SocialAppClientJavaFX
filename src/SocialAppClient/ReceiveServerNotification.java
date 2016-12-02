561d5c462f53207edf61af7bbe9a069819bfc299
kareem <karim.salah2048@gmail.com>
2016-12-03 00:12:51 +0200

    add notifications feature

--- src/SocialAppClient/ReceiveServerNotification.java
+++ src/SocialAppClient/ReceiveServerNotification.java
@@ -4,6 +4,7 @@ import SocialAppGeneral.Command;
 import SocialAppGeneral.ReceiveCommand;
 
 import java.net.Socket;
+import java.net.SocketException;
 
 /**
  * Created by kemo on 28/10/2016.
@@ -12,10 +13,16 @@ public class ReceiveServerNotification extends ReceiveCommand {
     public ReceiveServerNotification(Socket remote)
     {
         super(remote);
+        try {
+            remote.setSoTimeout(0);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public void Analyze(Command command) {
         //TODO #kareem
         //our code starts here HF :)
+        System.out.println("Notification says:"+ command.toString());
     }
 }
--- src/SocialAppGeneral/Like.java
+++ src/SocialAppGeneral/Like.java
@@ -1,9 +1,11 @@
 package SocialAppGeneral;
 
+import java.io.Serializable;
+
 /**
  * Created by khaled hesham on 12/2/2016.
  */
-public class Like extends PostAtachmment {
+public class Like extends PostAtachmment implements Serializable {
     private int like;
 
     public int getLike() {
@@ -13,5 +15,4 @@ public class Like extends PostAtachmment {
     public void setLike(int like) {//put 1 if put like put -1 if put dislike
         this.like = like;
     }
-
 }
