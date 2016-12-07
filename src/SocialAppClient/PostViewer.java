1ec2dee2a37089402fe44a0748a14e6f8a06d524
Billy <billy.fcih@yahoo.com>
2016-12-07 13:44:56 +0200

    Add repost feature

--- src/SocialAppClient/PostViewer.java
+++ src/SocialAppClient/PostViewer.java
@@ -1,6 +1,7 @@
 package SocialAppClient;
 
 import SocialAppGeneral.Command;
+import SocialAppGeneral.Like;
 import SocialAppGeneral.Post;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -26,6 +27,7 @@ public class PostViewer extends VBox {
     private Post post;
     public PostViewer(Post post){
         this.post = post;
+        System.out.println(post.convertToJsonString());
         setLayout();
     }
     private void setLayout(){
@@ -38,44 +40,21 @@ public class PostViewer extends VBox {
         postText.setFont(Font.font(16));
         postText.setWrapText(true);
         postText.setPadding(new Insets(0,0,10,0));
-
+/*
         Image im = new Image("file:C:\\Users\\bolla\\Pictures\\me.jpg");
         ImageView img = new ImageView(im);
         img.setFitWidth(350);
         img.setPreserveRatio(true);
         img.setSmooth(true);
         img.setCache(true);
-
+*/
         ImageView TUicon = new ImageView("file:Resources/TU.png");
         TUicon.setFitWidth(15);
         TUicon.setPreserveRatio(true);
-        int i=0;
-        while (i<post.getLike().size()&&post.getLike().get(i).getOwnerID() == Long.parseLong(MainWindow.id)){
-            i++;
-        }
-        /*
-
-        Command command = new Command();
-                    command.setKeyWord(Post.EDITE_POST);
-                    command.setSharableObject(post);
-        command.setSharableObject();
-                    CommandRequest commandRequest = new CommandRequest(MainServerConnection.mainConnectionSocket,command) {
-                        @Override
-                        void analyze(Command cmd) {
-                            if (cmd.getKeyWord().equals(Post.SAVE_POST_USER)) {
-
-
-                            }
-                        }
-                    };
-                    CommandsExecutor.getInstance().add(commandRequest);
-         */
 
         thumbsUp = new Button("Thumb UP", TUicon);
-        thumbsUp.setStyle("-fx-font: 12 arial; -fx-background-color: #ffffff; -fx-text-fill: #000000;");
-        thumbsUp.setOnMouseEntered(event -> thumbsUp.setStyle("-fx-background-color: #999999; -fx-text-fill: #000000;"));
-        thumbsUp.setOnMouseExited(event -> thumbsUp.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;"));
 
+        /*
         int finalI = i;
         thumbsUp.setOnMouseClicked(event -> {
             if( finalI ==0 ||post.getLike().get(finalI).getLike()==0  ) {
@@ -91,16 +70,14 @@ public class PostViewer extends VBox {
                 thumbsUp.setOnMouseEntered(event1 -> thumbsUp.setStyle("-fx-background-color: #999999; -fx-text-fill: #000000;"));
                 thumbsUp.setOnMouseExited(event1 -> thumbsUp.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;"));
             }
-        });
+        });*/
         ImageView TDicon = new ImageView("file:Resources/TD.png");
         TDicon.setFitWidth(15);
         TDicon.setPreserveRatio(true);
 
         thumbsDown = new Button("Thumb Down", TDicon);
-        thumbsDown.setStyle("-fx-font: 12 arial; -fx-background-color: #ffffff; -fx-text-fill: #000000;");
-        thumbsDown.setOnMouseEntered(event -> thumbsDown.setStyle("-fx-background-color: #999999; -fx-text-fill: #000000;"));
-        thumbsDown.setOnMouseExited(event -> thumbsDown.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;"));
 
+        /*
         thumbsDown.setOnMouseClicked(event -> {
             if(finalI ==0 ||post.getLike().get(finalI).getLike()==0) {
                 thumbsDown.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #ff0000");
@@ -116,6 +93,46 @@ public class PostViewer extends VBox {
                 thumbsDown.setOnMouseExited(event1 -> thumbsDown.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;"));
             }
         });
+*/
+
+        int i = 0;
+        int check = -1;
+        while (i<post.getLike().size()&&post.getLike().get(i).getOwnerID() != Long.parseLong(MainWindow.id)){
+            if(post.getLike().get(i).getOwnerID() == Long.parseLong(MainWindow.id)) {
+                check = i;
+            }
+            i++;
+        }
+        if(check == -1) {
+            setLikeStyle(check);
+            thumbsUp.setOnMouseClicked(event -> {
+                setLikeStyle(1);
+                Like like = new Like();
+                like.setLike(1);
+                like.setOwnerID(Long.parseLong(MainWindow.id));
+                Post post1 = new Post();
+                post1.setId(post.getId());
+                post1.setPostPos(post.getPostPos());
+                post1.addlike(like);
+                Command command = new Command();
+                command.setKeyWord(Post.EDIT_POST_USERS);
+                command.setSharableObject(post1.convertToJsonString());
+                CommandRequest commandRequest = new CommandRequest(MainServerConnection.mainConnectionSocket,command) {
+                    @Override
+                    void analyze(Command cmd) {
+                        if (cmd.getKeyWord().equals(Post.EDIT_POST_USERS)) {
+
+                        }
+                    }
+                };
+                CommandsExecutor.getInstance().add(commandRequest);
+            });
+
+        }else if(post.getLike().get(check).getLike() == 1){
+            setLikeStyle(1);
+        }else{
+            setLikeStyle(0);
+        }
 
         ImageView commenticon = new ImageView("file:Resources/comment.png");
         commenticon.setFitWidth(15);
@@ -130,18 +147,63 @@ public class PostViewer extends VBox {
         shareicon.setFitWidth(15);
         shareicon.setPreserveRatio(true);
 
-        share = new Button("Share" ,shareicon);
+        share = new Button("Repost" ,shareicon);
         share.setStyle("-fx-font: 12 arial; -fx-background-color: #ffffff; -fx-text-fill: #000000;");
         share.setOnMouseEntered(event -> share.setStyle("-fx-background-color: #999999; -fx-text-fill: #000000;"));
         share.setOnMouseExited(event -> share.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;"));
+        share.setOnMouseClicked(event -> {
+            Post post1=new Post();
+            post1.setOwnerId(Long.parseLong(MainWindow.id));
+            post1.setContent(post.getContent());
+            post1.setPostPos(Long.parseLong(MainWindow.id));
+            Command command = new Command();
+            command.setKeyWord(Post.SAVE_POST_USER);
+            command.setSharableObject(post1.convertToJsonString());
+            CommandRequest commandRequest = new CommandRequest(MainServerConnection.mainConnectionSocket,command) {
+                @Override
+                void analyze(Command cmd) {
+                    if (cmd.getKeyWord().equals(Post.SAVE_POST_USER)) {
+
+                    }
+                }
+            };
+            CommandsExecutor.getInstance().add(commandRequest);
+        });
 
         HBox buttons = new HBox(thumbsUp, thumbsDown, comment, share);
         buttons.setAlignment(Pos.CENTER);
 
         setMaxWidth(400);
         setMargin(postText, new Insets(0,30,0,30));
-        setMargin(img, new Insets(10,0,20,0));
-        getChildren().addAll(new FriendView(""+post.getOwnerId()), postText, img, new Separator(), buttons);
+        //setMargin(img, new Insets(10,0,20,0));
+        getChildren().addAll(new FriendView(""+post.getOwnerId()), postText, /*img,*/ new Separator(), buttons);
 
+    }
+    private void setLikeStyle(int i){
+        if(i == -1) {
+            thumbsUp.setStyle("-fx-font: 12 arial; -fx-background-color: #ffffff; -fx-text-fill: #000000;");
+            thumbsUp.setOnMouseEntered(event -> thumbsUp.setStyle("-fx-background-color: #999999; -fx-text-fill: #000000;"));
+            thumbsUp.setOnMouseExited(event -> thumbsUp.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;"));
+            thumbsDown.setStyle("-fx-font: 12 arial; -fx-background-color: #ffffff; -fx-text-fill: #000000;");
+            thumbsDown.setOnMouseEntered(event -> thumbsDown.setStyle("-fx-background-color: #999999; -fx-text-fill: #000000;"));
+            thumbsDown.setOnMouseExited(event -> thumbsDown.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;"));
+
+
+        }else if(i == 1){
+            thumbsUp.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #0000ff");
+            thumbsUp.setOnMouseEntered(event -> thumbsUp.setStyle("-fx-background-color: #999999; -fx-text-fill: #0000ff"));
+            thumbsUp.setOnMouseExited(event -> thumbsUp.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #0000ff"));
+            thumbsDown.setStyle("-fx-font: 12 arial; -fx-background-color: #ffffff; -fx-text-fill: #000000;");
+            thumbsDown.setOnMouseEntered(event -> thumbsDown.setStyle("-fx-background-color: #999999; -fx-text-fill: #000000;"));
+            thumbsDown.setOnMouseExited(event -> thumbsDown.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;"));
+        }else{
+            thumbsDown.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #ff0000");
+            thumbsDown.setOnMouseEntered(event -> thumbsDown.setStyle("-fx-background-color: #999999; -fx-text-fill: #ff0000"));
+            thumbsDown.setOnMouseExited(event -> thumbsDown.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #ff0000"));
+            thumbsUp.setStyle("-fx-font: 12 arial; -fx-background-color: #ffffff; -fx-text-fill: #000000;");
+            thumbsUp.setOnMouseEntered(event -> thumbsUp.setStyle("-fx-background-color: #999999; -fx-text-fill: #000000;"));
+            thumbsUp.setOnMouseExited(event -> thumbsUp.setStyle("-fx-background-color: #ffffff; -fx-text-fill: #000000;"));
+
+        }
     }
 }
