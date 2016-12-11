ba60507c5ce9add15d8c01c957b1bf4aa8b8c9bb
Billy <billy.fcih@yahoo.com>
2016-12-11 13:43:53 +0200

    Working on chat

---
+++ src/SocialAppClient/ChatWindow.java
@@ -0,0 +1,11 @@
+package SocialAppClient;
+
+/**
+ * Created by billy on 2016-12-11.
+ */
+public class ChatWindow {
+    private String id;
+    public ChatWindow(String id){
+        this.id = id;
+    }
+}
