0c60a448b08095c9096d75570756df89135efc6f
mostafa7azem <mostafahazem144@yahoo.com>
2016-12-15 10:13:35 +0200

    admin done

---
+++ src/SocialAppClient/ClientAdmin.java
@@ -0,0 +1,81 @@
+package SocialAppClient;
+
+import SocialAppGeneral.Admin;
+import SocialAppGeneral.Command;
+import SocialAppGeneral.SocialArrayList;
+
+/**
+ * Created by mosta on 13-Dec-16.
+ */
+public class ClientAdmin extends ClientLoggedUser implements Admin {
+
+    public ClientAdmin(String id) {
+        super(id);
+    }
+
+    @Override
+    public void approve(String Email) {
+        Command command = new Command();
+        command.setKeyWord(Admin.APPROVE);
+        command.setSharableObject(Email);
+        CommandRequest commandRequest = new CommandRequest(MainServerConnection.mainConnectionSocket, command) {
+            @Override
+            void analyze(Command commandFromServer) {
+                System.out.println(commandFromServer.getObjectStr());
+
+            }
+        };
+        CommandsExecutor.getInstance().add(commandRequest);
+
+    }
+
+    @Override
+    public void reject(String Email) {
+        Command command = new Command();
+        command.setKeyWord(Admin.REJECT);
+        command.setSharableObject(Email);
+        CommandRequest commandRequest = new CommandRequest(MainServerConnection.mainConnectionSocket, command) {
+            @Override
+            void analyze(Command commandFromServer) {
+                System.out.println(commandFromServer.getObjectStr());
+
+            }
+        };
+        CommandsExecutor.getInstance().add(commandRequest);
+    }
+
+    @Override
+    public void approveAsAdmin(String Email) {
+        Command command = new Command();
+        command.setKeyWord(Admin.APPROVEASADMIN);
+        command.setSharableObject(Email);
+        CommandRequest commandRequest = new CommandRequest(MainServerConnection.mainConnectionSocket, command) {
+            @Override
+            void analyze(Command commandFromServer) {
+                System.out.println(commandFromServer.getObjectStr());
+
+            }
+        };
+        CommandsExecutor.getInstance().add(commandRequest);
+    }
+    public abstract class FetchRequests
+    {
+        public FetchRequests()
+        {
+            Command command = new Command();
+            command.setKeyWord(Admin.RetrieveData);
+            CommandRequest commandRequest = new CommandRequest(MainServerConnection.mainConnectionSocket, command) {
+                @Override
+                void analyze(Command commandFromServer) {
+                    System.out.println(commandFromServer.getObjectStr());
+                    SocialArrayList socialArrayList = SocialArrayList.convertFromJsonString(commandFromServer.getObjectStr());
+                    onRetrieve(socialArrayList);
+                }
+            };
+            CommandsExecutor.getInstance().add(commandRequest);
+
+
+        }
+        public abstract void onRetrieve(SocialArrayList list);
+    }
+}
---
+++ src/SocialAppGeneral/Admin.java
@@ -0,0 +1,16 @@
+package SocialAppGeneral;
+
+
+/**
+ * Created by mosta on 27-Nov-16.
+ */
+public abstract interface Admin {
+    public static  final String KEYWORD="ADMIN_CHECK";
+    public static  final String RetrieveData="RETRIEVE_DATA";
+    public static final  String APPROVE="APPROVE";
+    public static final  String APPROVEASADMIN="APPROVE_AS_ADMIN";
+    public static final  String REJECT="REJECT";
+    public abstract void approve(String Email);
+    public abstract void reject(String Email);
+    public abstract void approveAsAdmin(String Email);
+}
