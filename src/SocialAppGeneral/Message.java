7bb87c08845121dfa2a5d6b0a15748f770dd7161
kareem <karim.salah2048@gmail.com>
2016-12-16 00:58:36 +0200

    finish 1 to 1 chat

--- src/SocialAppGeneral/Message.java
+++ src/SocialAppGeneral/Message.java
@@ -7,13 +7,16 @@ import com.google.gson.Gson;
  */
 public class Message implements Shareable {
     public final static String FETCH_MESSAGES = "fetch_messages";
+    public final static String SEND_MESSAGE = "send_message";
+    public final static String RECEIVE_MESSAGE = "receive_message";
     private String Sender;
     private String Message;
+    private String Receiver;
     @Override
     public String convertToJsonString() {
         return new Gson().toJson(this);
     }
-    public static Message FromJson(String Json)
+    public static SocialAppGeneral.Message FromJson(String Json)
     {
         return new Gson().fromJson(Json, SocialAppGeneral.Message.class);
     }
@@ -32,5 +35,13 @@ public class Message implements Shareable {
 
     public void setMessage(String message) {
         Message = message;
+    }
+
+    public String getReceiver() {
+        return Receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        Receiver = receiver;
     }
 }
