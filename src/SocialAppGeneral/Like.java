875e2dfc9f5655f199aeef6ab4e2e16604bee6bd
Billy <billy.fcih@yahoo.com>
2016-12-18 14:59:13 +0200

    Change like and comment implemntation

--- src/SocialAppGeneral/Like.java
+++ src/SocialAppGeneral/Like.java
@@ -6,13 +6,13 @@ import java.io.Serializable;
  * Created by khaled hesham on 12/2/2016.
  */
 public class Like extends PostAtachmment implements Serializable {
-    private int like;
-
-    public int getLike() {
+    private Relations like;
+    private static final long serialVersionUID = 6529685098267757690L;
+    public Relations getLike() {
         return like;
     }
 
-    public void setLike(int like) {//put 1 if put like put -1 if put dislike
+    public void setLike(Relations like) {//put 1 if put like put -1 if put dislike
         this.like = like;
     }
 }
